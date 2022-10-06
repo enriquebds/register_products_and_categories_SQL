f12bc8c6716a66ef45a2614600a57317233e78cc
Enrique <enrique.barbosasilva@gmail.com>
2022-10-06 18:29:50 -0300

    feat: changes-made

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 import express from "express";
 import "dotenv/config";
+import productsAndCategoriesRoutes from "./routes/productsAndCategories.routes";
+import { startDatabase } from "./database";
 
 const app = express();
 
 app.use(express.json());
+app.use(productsAndCategoriesRoutes);
 
 export default app.listen(3333, () => {
-  console.log("Server running");
+  startDatabase();
 });
