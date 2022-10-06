589d41280ea975518da909e5302574b38652ae25
Enrique <enrique.barbosasilva@gmail.com>
2022-10-06 18:59:45 -0300

    feat: createCategorie-controller-created

---
+++ src/controllers/createCategorie.controller.js
@@ -0,0 +1,17 @@
+import createCategorie from "../services/createCategorie";
+
+const createCategorieController = async (req, res) => {
+  const { name } = req.body;
+
+  try {
+    const categorie = await createCategorie(name);
+
+    return res.status(201).json(categorie);
+  } catch (error) {
+    return res.status(400).json({
+      message: error.message,
+    });
+  }
+};
+
+export default createCategorieController;
