196dcb2deb8d013971a3ad94f1ea5466c8661b1b
Enrique <enrique.barbosasilva@gmail.com>
2022-10-06 18:28:30 -0300

    feat: listCategories-service-created

---
+++ src/services/listAllCategories.js
@@ -0,0 +1,13 @@
+import database from "../database";
+
+const listAllCategories = async () => {
+  try {
+    const res = await database.query("SELECT * FROM categories");
+
+    return res.rows;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export default listAllCategories;
