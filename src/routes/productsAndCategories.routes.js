4e994d53ffb35015e817c412e4201511ff0d5aef
Enrique <enrique.barbosasilva@gmail.com>
2022-10-06 19:00:13 -0300

    feat: createCategorie-route-post-created

--- src/routes/productsAndCategories.routes.js
+++ src/routes/productsAndCategories.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import createCategorieController from "../controllers/createCategorie.controller";
 import listAllCategoriesController from "../controllers/listAllCategories.controller";
 
 const routes = Router();
 
 routes.get("/categories", listAllCategoriesController);
+routes.post("/categories", createCategorieController);
 
 export default routes;
