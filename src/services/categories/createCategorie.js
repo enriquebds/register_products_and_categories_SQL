930d299f2ba3a4cef2d55b2388b3dcd92d75401a
Enrique <enrique.barbosasilva@gmail.com>
2022-10-07 00:34:44 -0300

    docs: category-service-files-placed-inside-a-folder

---
+++ src/services/categories/createCategorie.js
@@ -0,0 +1,15 @@
+import database from "../../database";
+
+const createCategorie = async (name) => {
+  try {
+    const res = await database.query(
+      "INSERT INTO categories(name) VALUES($1) RETURNING *",
+      [name]
+    );
+    return res.rows[0];
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export default createCategorie;
