6b318199a041106838ee2e2e62d9f241c3de98ab
Enrique <enrique.barbosasilva@gmail.com>
2022-10-07 17:37:41 -0300

    feat: listCategoryById-route-created

--- src/routes/categories.routes.js
+++ src/routes/categories.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import createCategorieController from "../controllers/categories.controllers/categories/createCategorie.controller";
 import listAllCategoriesController from "../controllers/categories.controllers/categories/listAllCategories.controller";
+import listCategoryByIdController from "../controllers/categories.controllers/categories/listCategoryById.controller";
 
 const routes = Router();
 
-routes.get("/categories", listAllCategoriesController);
-routes.post("/categories", createCategorieController);
+routes.get("", listAllCategoriesController);
+routes.post("", createCategorieController);
+routes.get("/:id", listCategoryByIdController);
 
 export default routes;
