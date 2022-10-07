eda15c46325d119bffe59fce4956810451341e0b
Enrique <enrique.barbosasilva@gmail.com>
2022-10-07 15:53:10 -0300

    feat: get-route-created

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import createProductController from "../controllers/products/createProducts.controller";
+import listAllProductsController from "../controllers/products/listAllProducts.controller";
 
 const routes = Router();
 
+routes.get("/products", listAllProductsController);
 routes.post("/products", createProductController);
 
 export default routes;
