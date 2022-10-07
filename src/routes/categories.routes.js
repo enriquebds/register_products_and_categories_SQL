b85b959982b0a325d99f9613c6f859db9c06e6e7
Enrique <enrique.barbosasilva@gmail.com>
2022-10-07 00:37:15 -0300

    refactor: changed-files

--- src/routes/categories.routes.js
+++ src/routes/categories.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import createCategorieController from "../controllers/createCategorie.controller";
-import listAllCategoriesController from "../controllers/listAllCategories.controller";
+import createCategorieController from "../controllers/controllers/categories/createCategorie.controller";
+import listAllCategoriesController from "../controllers/controllers/categories/listAllCategories.controller";
 
 const routes = Router();
 
