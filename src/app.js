d1832a268dcf4b0218ffdbde125cd0e02bcdafce
Enrique <enrique.barbosasilva@gmail.com>
2022-10-07 15:42:02 -0300

    refactor: code-changed

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ import express from "express";
 import "dotenv/config";
 import categoriesRoutes from "./routes/categories.routes";
 import { startDatabase } from "./database";
+import productsRoutes from "./routes/products.routes";
 
 const app = express();
 
 app.use(express.json());
 app.use(categoriesRoutes);
+app.use(productsRoutes);
 
 export default app.listen(3333, () => {
   startDatabase();
