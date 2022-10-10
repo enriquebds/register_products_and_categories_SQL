5f8b5ab550276204189d5e51615f90e15e699366
Enrique <enrique.barbosasilva@gmail.com>
2022-10-10 18:18:37 -0300

    refactor: code-changed

--- src/controllers/products/updateProduct.controller.js
+++ src/controllers/products/updateProduct.controller.js
@@ -2,11 +2,14 @@ import updateProduct from "../../services/products/updateProduct";
 
 const updateProductController = async (req, res) => {
   const { id } = req.params;
-  const { name, price, category_id } = req.body;
+  const data = req.body;
   try {
-    const updatedProduct = await updateProduct(id, name, price, category_id);
+    const updatedProduct = await updateProduct(id, data);
 
-    return res.status(200).json(updatedProduct);
+    return res.status(200).json({
+      message: "Product updated",
+      product: updatedProduct,
+    });
   } catch (error) {
     return res.status(400).json({
       message: error.message,
