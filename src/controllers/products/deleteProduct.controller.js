e8024a1f51b50562c0134e41c74528831b26e055
Enrique <enrique.barbosasilva@gmail.com>
2022-10-10 17:31:23 -0300

    refactor: code-changed

--- src/controllers/products/deleteProduct.controller.js
+++ src/controllers/products/deleteProduct.controller.js
@@ -3,11 +3,13 @@ import deleteProduct from "../../services/products/deleteProduct";
 const deleteProductController = async (req, res) => {
   const { id } = req.params;
   try {
-    const deleted = await deleteProduct(id);
+    await deleteProduct(id);
 
-    return res.status(204).json(deleted);
+    return res.status(204).send();
   } catch (error) {
-    return res.status(400).json(error.message);
+    return res.status(400).json({
+      message: error.message,
+    });
   }
 };
 
