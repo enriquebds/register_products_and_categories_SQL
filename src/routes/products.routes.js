d85e5e347c58ba769c9ae540348df2cc9bb94386
Enrique <enrique.barbosasilva@gmail.com>
2022-10-10 13:29:33 -0300

    feat: listProductById-route-created

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import createProductController from "../controllers/products/createProducts.controller";
 import listAllProductsController from "../controllers/products/listAllProducts.controller";
+import listProductByIdController from "../controllers/products/listProductById.controller";
 
 const routes = Router();
 
 routes.get("", listAllProductsController);
 routes.post("", createProductController);
+routes.get("/:id", listProductByIdController);
 
 export default routes;
