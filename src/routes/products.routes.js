092ec43d5f01ede28c5864c0199a3722ecfeeaf7
Enrique <enrique.barbosasilva@gmail.com>
2022-10-10 14:42:12 -0300

    feat: updateProduct-route-added

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import createProductController from "../controllers/products/createProducts.controller";
 import listAllProductsController from "../controllers/products/listAllProducts.controller";
 import listProductByIdController from "../controllers/products/listProductById.controller";
+import updateProductController from "../controllers/products/updateProduct.controller";
 
 const routes = Router();
 
 routes.get("", listAllProductsController);
 routes.post("", createProductController);
 routes.get("/:id", listProductByIdController);
+routes.patch("/:id", updateProductController);
 
 export default routes;
