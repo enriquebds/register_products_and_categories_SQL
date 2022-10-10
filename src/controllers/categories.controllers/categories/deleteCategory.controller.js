308867d76e7a580e02fa1dba3399829b1f0ac0b0
Enrique <enrique.barbosasilva@gmail.com>
2022-10-10 17:30:27 -0300

    refactor: code-changed

--- src/controllers/categories.controllers/categories/deleteCategory.controller.js
+++ src/controllers/categories.controllers/categories/deleteCategory.controller.js
@@ -3,11 +3,13 @@ import deleteCategory from "../../../services/categories/deleteCategory";
 const deleteCategoryController = async (req, res) => {
   const { id } = req.params;
   try {
-    const deleted = await deleteCategory(id);
+    await deleteCategory(id);
 
-    return res.status(204).json(deleted);
+    return res.status(204).send();
   } catch (error) {
-    return res.status(400).json(error.message);
+    return res.status(400).json({
+      message: error.message,
+    });
   }
 };
 
