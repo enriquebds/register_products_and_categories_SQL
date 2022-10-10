e026bd0d4ddd0a9bf66c1433625c6c0a522acfa2
Enrique <enrique.barbosasilva@gmail.com>
2022-10-10 12:50:03 -0300

    feat: updateCategory-service-created

---
+++ src/services/categories/updateCategory.js
@@ -0,0 +1,20 @@
+import database from "../../database";
+
+const updateCategory = async (id, name) => {
+  try {
+    const res = await database.query(
+      "UPDATE categories SET name = $1 WHERE id = $2 RETURNING *",
+      [name, id]
+    );
+
+    if (res.rowCount === 0) {
+      throw new Error("Category not found");
+    }
+
+    return { message: "Atualizado", category: res.rows[0] };
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export default updateCategory;
