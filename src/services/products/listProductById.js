051392e1f4f00b251e199bb5a6bf12ef24d4f150
Enrique <enrique.barbosasilva@gmail.com>
2022-10-10 13:28:13 -0300

    feat: listProductById-service-created

---
+++ src/services/products/listProductById.js
@@ -0,0 +1,20 @@
+import database from "../../database";
+
+const listProductById = async (id) => {
+  try {
+    const res = await database.query("SELECT * FROM products WHERE id = $1", [
+      id,
+    ]);
+
+    const product = res.rows[0];
+
+    if (!product) {
+      throw new Error("Product not found");
+    }
+
+    return product;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+export default listProductById;
